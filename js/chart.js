18fc6383cdaf71f2234b4b96b97fb5bb987c8536
Breunk86 <brukantabula@gmail.com>
2019-04-24 22:20:21 +0200

    chart js refactored

--- js/chart.js
+++ js/chart.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const dailyTrafficChart = document.getElementById('bar-chart').getContext('2d');
     const mobileUserChart = document.getElementById('doughnut-chart').getContext('2d');
     const trafficChartMenu = document.querySelector('.traffic-chart-menu');
+    const chartMenuItems = trafficChartMenu.querySelectorAll('p');
 
     const hourlyLabels = ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00'];
     const hourlyData =  [1500, 2550, 1750, 3500, 2000, 1500, 2500, 1250, 1800, 500, 1700, 2700, 1500];
@@ -18,44 +19,34 @@ document.addEventListener("DOMContentLoaded", () => {
     const dailyTrafficData =  [75, 100, 175, 125, 225, 200, 100];
     const mobileUserLabels = ['Tablet','Phone', 'Desktop'];
     const mobileUserData =  [20, 25, 145];
-
-    const selectItems = (parent, item) => parent.querySelectorAll(item);
-
+    const defaultChartList = ['weekly' , 'dailyTraffic', 'mobileUser'];
     
-    const createChart = (labels, data, bgOpacity = .3, brdrOpacity = .7, chartName = trafficChart, type = 'line', isDoughnut = false) => {
-        const max = Math.max.apply(null,  data);
-        const stepSize = Math.min.apply(null,  data);
-
+    const createChart = (labels, data, bgOpacity = .3, chartName = trafficChart, type = 'line', isDoughnut = false) => {
         new Chart(chartName, {
-            type: type,
-            data: {
-                labels: labels,
-                datasets: [{
-                    data: data,
-                    backgroundColor: isDoughnut ? [ '#81c98f', '#74b1bf','#7377bf'] : `rgba(115, 119, 191, ${bgOpacity})`,
-                    borderColor: `rgba(115, 119, 191, ${brdrOpacity})`,
-                    borderWidth: .5
-                }]
+          type: type,
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                data: data,
+                backgroundColor: isDoughnut
+                  ? ["#81c98f", "#74b1bf", "#7377bf"]
+                  : `rgba(115, 119, 191, ${bgOpacity})`,
+                borderColor: "#7377bf",
+                borderWidth: 0.5
+              }
+            ]
+          },
+          options: {
+            maintainAspectRatio: false,
+            responsive: true,
+            legend: {
+              display: isDoughnut ? true : false,
+              position: "right",
+              labels: { boxWidth: 15, fontSize: 16 }
             },
-            options: {
-                scales: {
-                    yAxes: [{ ticks: { 
-                        beginAtZero: true,
-                         stepSize: isDoughnut ? 50 : stepSize, max: isDoughnut ? max + 300 : max
-                        }
-                    }],
-                    xAxes: [{ ticks: { maxTicksLimit: data.length } }]
-                },
-                maintainAspectRatio: false,
-                responsive: true,
-                legend: { display: isDoughnut ? true : false , position: 'right',
-                labels: { boxWidth: 15, fontSize: 16}
-                },
-                elements: {
-                line: { tension: 0 },
-                point: { radius: 5 }
-            }
-        }
+            elements: { line: { tension: 0 }, point: { radius: 5 } }
+          }
         });
     };
 
@@ -64,22 +55,20 @@ document.addEventListener("DOMContentLoaded", () => {
             daily: () => createChart(dailyLabels, dailyData),
             weekly: () => createChart(weeklyLabels, weeklyData),
             monthly: () => createChart(monthlyLabels, monthlyData),
-            dailyTraffic: () => createChart(dailyTrafficLabels, dailyTrafficData, 1, .8, dailyTrafficChart, 'bar'),
-            mobileUser: () => createChart (mobileUserLabels, mobileUserData, .8, .7, mobileUserChart, 'doughnut', true)
+            dailyTraffic: () => createChart(dailyTrafficLabels, dailyTrafficData, 1, dailyTrafficChart, 'bar'),
+            mobileUser: () => createChart (mobileUserLabels, mobileUserData, .8, mobileUserChart, 'doughnut', true)
         };
 
-        viewChart['weekly']();
-        viewChart['dailyTraffic']();
-        viewChart['mobileUser']();
+        defaultChartList.forEach(type => viewChart[type]());
     
         trafficChartMenu.addEventListener("click", e => {
           const selectedItem = e.target.className;
 
           viewChart[selectedItem]();
 
-          selectItems(trafficChartMenu, "p").forEach(element => {
-            element.id =
-              selectedItem == element.className
+          chartMenuItems.forEach(item => {
+            item.id =
+              selectedItem == item.className
                 ? "item-selected"
                 : "item-deselected";
           });
